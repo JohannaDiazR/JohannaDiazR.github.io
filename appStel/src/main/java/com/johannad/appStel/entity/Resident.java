664b2536e3726eab11300b93586b1aaf4e69e170
Johanna Diaz <123778194+JohannaDiazR@users.noreply.github.com>
2024-03-02 19:07:44 -0500

    Update Resident.java
    Actualización Entity Resident

--- appStel/src/main/java/com/johannad/appStel/entity/Resident.java
+++ appStel/src/main/java/com/johannad/appStel/entity/Resident.java
@@ -1,9 +1,12 @@
 package com.johannad.appStel.entity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -11,44 +14,39 @@ import java.util.List;
 @Table(name = "tblResidente")
 @Data
 @AllArgsConstructor
-@NoArgsConstructor    
+@NoArgsConstructor
 public class Resident implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "nomResidente", length = 35)
+    @Column(name = "nom_residente", length = 35)
     private String nomResidente;
 
-    @Column(name = "cedResidente", length = 11)
+    @Column(name = "ced_residente", length = 11)
     private int cedResidente;
 
-    @Column(name = "emaResidente", length = 40)
+    @Column(name = "ema_residente", length = 40)
     private String emaResidente;
 
-    @Column(name = "celResidente", length = 11)
-    private int celResidente;
+    @Column(name = "cel_residente", length = 11)
+    private long celResidente;
 
-    @Column(name = "mascResidente")
-    private boolean mascResidente;
+    @Column(name = "num_integrantes")
+    private int numIntegrantes;
 
-    @Column(name = "nmascResidente", length = 11)
-    private int nmascResidente;
-
-    @Column(name = "fnacResidente")
-    private Date fnacResidente;
-
-    @Column(name = "edadResidente", length = 11)
-    private int edadResidente;
-
+    @JsonManagedReference
     @ManyToOne
-    @JoinColumn (name = "fkidParqueadero")
+    @JoinColumn (name = "fkid_parqueadero")
     private Parking parking;
 
+    @JsonManagedReference
     @ManyToOne
-    @JoinColumn(name = "fkidRol")
+    @JoinColumn(name = "fkid_rol")
     private Role role;
 
-
+    @JsonBackReference
+    @OneToMany (mappedBy = "resident")
+    private List<Property> propertyList;
 
 }
