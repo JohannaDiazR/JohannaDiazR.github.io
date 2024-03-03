082ac5b95ca768b23dd2679b87c8e0750b5a5335
Johanna Diaz <123778194+JohannaDiazR@users.noreply.github.com>
2024-03-02 19:11:10 -0500

    Update WalletStatusService.java
    Actualización Wallet Status Service

--- appStel/src/main/java/com/johannad/appStel/service/WalletStatusService.java
+++ appStel/src/main/java/com/johannad/appStel/service/WalletStatusService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface WalletStatusService {
     public List<WalletStatus> findAll() throws Exception;
     public WalletStatus findById(int id);
-    public void create(WalletStatus walletStatus);
+    public WalletStatus create(WalletStatus walletStatus);
     public void update(WalletStatus walletStatus);
     public void delete(WalletStatus walletStatus);
 }
