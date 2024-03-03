2982a371ad93f0414927325509fa6e24e2d568ca
David-olaya <124094645+David-olaya@users.noreply.github.com>
2024-03-02 19:00:04 -0500

    Update Correspondence.java
    Codigo Actualizado Correspondence

--- appStel/src/main/java/com/johannad/appStel/entity/Correspondence.java
+++ appStel/src/main/java/com/johannad/appStel/entity/Correspondence.java
@@ -1,10 +1,13 @@
 package com.johannad.appStel.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//import javax.persistence.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -13,28 +16,28 @@ import java.util.List;
 @Table(name = "tbl_correspondencia")
 @Data
 @AllArgsConstructor
-@NoArgsConstructor    
+@NoArgsConstructor
 public class Correspondence  implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     int id;
-    @Column(name = "tipoCorrespondencia", length = 30)
-    String tipoCorrespondencia; //Tipo correspondencia paquete/carta/recibo
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "frecCorrespodencia")
-    Date frecCorrespondencia; //Fecha en la que ingresa la correspondencia al conjunto
-    @Column(name = "estCorrespondencia", length = 20)
-    String estCorrespondencia; //Estado de correspondencia entregado/no entregado
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "fentrCorrespondencia")
-    Date fentrCorrespondencia; //Fecha en la que se entrega la correspondencia
+    @Column(name = "tipo_correspondencia", length = 30)
+    private String tipoCorrespondencia; //Tipo correspondencia paquete/carta/recibo
+    //@Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "frec_correspodencia")
+    private Date frecCorrespondencia; //Fecha en la que ingresa la correspondencia al conjunto
+    @Column(name = "est_correspondencia", length = 20)
+    private String estCorrespondencia; //Estado de correspondencia entregado/no entregado
+    //@Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fentr_correspondencia")
+    private Date fentrCorrespondencia; //Fecha en la que se entrega la correspondencia
 
-    //@JsonIgnore
+    @JsonManagedReference
     @ManyToOne
-    @JoinColumn(name = "fkidTrabajador")
+    @JoinColumn(name = "fkid_trabajador")
     private Worker worker;
-
+    @JsonBackReference
     @ManyToMany(mappedBy = "correspondence", fetch = FetchType.LAZY)
     private List<Property> propertyList;
 
