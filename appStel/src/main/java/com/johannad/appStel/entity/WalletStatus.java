4af7988609a01500ca7e2edb3496b3747bc6638b
Johanna Diaz <123778194+JohannaDiazR@users.noreply.github.com>
2024-03-02 19:02:40 -0500

    Update WalletStatus.java
    Actualización Entity Wallet Status

--- appStel/src/main/java/com/johannad/appStel/entity/WalletStatus.java
+++ appStel/src/main/java/com/johannad/appStel/entity/WalletStatus.java
@@ -1,7 +1,10 @@
 package com.johannad.appStel.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //import javax.persistence.*;
 import java.io.Serializable;
@@ -12,28 +15,30 @@ import java.util.List;
 @Table(name = "tbl_estcartera")
 @Data
 @AllArgsConstructor
-@NoArgsConstructor    
+@NoArgsConstructor
 public class WalletStatus implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     int id;
-    @Column(name = "docEstCartera", length = 50)
-    String docEstCartera; //documento del estado //paz y salvo/acuerdo de pago
-    @Column(name = "estCartera", length = 30)
-    String estCartera; //estado de cartera mora//paz y salvo
-    @Column(name = "taccEstCartera", length = 20)
-    String taccEstCartera; //tipo de acceso permitido/bloqueado
-    @Column(name = "notiEstCartera", length = 35)
-    String notiEstCartera; //Notificar al residente
+    @Column(name = "docest_cartera", length = 50)
+    String docestcartera; //documento del estado //paz y salvo/acuerdo de pago
+    @Column(name = "est_cartera", length = 30)
+    String estcartera; //estado de cartera mora//paz y salvo
+    @Column(name = "tacc_estcartera", length = 20)
+    String taccestcartera; //tipo de acceso permitido/bloqueado
+    @Column(name = "noti_estcartera", length = 35)
+    String notiestcartera; //Notificar al residente
 
     //Llaves foraneas
     //fkidInmueble
+    @JsonManagedReference
     @ManyToOne
-    @JoinColumn(name = "fkidInmueble")
+    @JoinColumn(name = "fkid_inmueble")
     private Property property;
 
     //fkidTrabajador
+    @JsonManagedReference
     @ManyToOne
-    @JoinColumn(name = "fkidTrabajador")
+    @JoinColumn(name = "fkid_trabajador")
     private Worker worker;
 }
